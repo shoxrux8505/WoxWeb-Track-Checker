dc767396015fe614a04f6fe90bbd8f2a4f2f8daf
Bettee0885 <b44786280@gamil.com>
2025-01-27 02:49:48 +0300

    done

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+
+
+  <h2>
+    <img src="../assets/img/WOXWEB LOGO.png" alt="LOGO">
+    Expense Tracker
+  </h2>
+
+</template>
+<style scoped>
+
+h2{
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  flex-direction: column;
+  row-gap: 20px;
+}
+h2 img{
+  width: 390px;
+  height: 100px;
+  transform: scale(0.5);
+  filter: drop-shadow(0 0 10px royalblue);
+}
+
+@media screen and (max-width: 390px) {
+  h1 img {
+
+    transform: scale(0.2);
+  }
+}
+</style>
---
+++ src/components/Tabs.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="tab-buttons">
+        <button v-for="(tab, index) in tabs" :key="index" @click="changeTab(index)">
+            {{ tab.label }}
+        </button>
+    </div>
+    <div class="tabs-content">
+        <component :is="currentTabComponent"></component>
+    </div>
+</template>
+
+<script>
+// import TransactionList from './components/TransactionList.vue';
+// import AddTransaction from './components/AddTransaction.vue';
+export default {
+    name: 'Tab',
+    components: {
+        TransactionList,
+        AddTransaction
+    },
+    data() {
+        return {
+            tabs: [
+                { label: 'History', component: TransactionList },
+                { label: 'AddTransaction', component: AddTransaction },
+            ],
+            activeTabIndex: 0,
+        };
+    },
+    computed: {
+        currentTabComponent() {
+            return this.tabs[this.activeTabIndex].component;
+        }
+    },
+    methods: {
+        changeTab(index) {
+            this.activeTabIndex = index;
+        },
+    },
+}
+</script>
+
+<style scoped></style>
